e572b8d6928ac6954ba207afe5eb94c536042971
Harish1651999 <hk1651999@gmail.com>
2023-11-09 08:52:11 +0530

    add custom component and css modules.

--- app/[meetupId]/page.js
+++ app/[meetupId]/page.js
@@ -1,5 +1,14 @@
-function DetailPage() {
-  return <h1>Hii</h1>;
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://img.freepik.com/free-vector/group-young-people-posing-photo_52683-18823.jpg?w=740&t=st=1699164200~exp=1699164800~hmac=358cb1893cf03ca7bcef7b7fd177dfc3ce4f65caabd9ba08c34a00fd5d1d1916"
+      title="A First Meetup"
+      address="Some address 5, 12345 Some City"
+      description="The meetup description"
+    />
+  );
 }
 
-export default DetailPage;
+export default MeetupDetails;
